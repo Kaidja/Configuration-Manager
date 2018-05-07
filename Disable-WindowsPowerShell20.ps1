30a81c873c6f85fbc2777ba8545cd905776bcd3d
Kaidja <Kaido1000@hotmail.com>
2018-05-07 19:21:35 +0300

    Delete Disable-WindowsPowerShell20.ps1

--- Disable-WindowsPowerShell20.ps1
+++
@@ -1,5 +0,0 @@
-#Discovery Script
-Get-WindowsOptionalFeature -FeatureName "MicrosoftWindowsPowerShellV2Root" -Online | Select-Object -ExpandProperty State
-
-#Remediation Script
-Disable-WindowsOptionalFeature -FeatureName "MicrosoftWindowsPowerShellV2Root" -Online 
